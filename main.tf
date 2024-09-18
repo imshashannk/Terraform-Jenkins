e4223294b6c7e7fd5726c1f543effe5e7df4ba17
Shashank Khankari <81563774+imshashannk@users.noreply.github.com>
2024-09-18 17:41:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,85 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "TF-VPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "TF-Public-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "TF-Internet-Gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "TF-Public-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main.id
+  name   = "allow_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TF-Security-Group"
+  }
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0e86e20dae9224db8" # us-west-2
-  instance_type = "t2.micro"
+  ami                         = "ami-08c40ec9ead489470"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  security_groups             = [aws_security_group.allow_ssh.name]
+  associate_public_ip_address = true
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.foo.public_ip
 }
